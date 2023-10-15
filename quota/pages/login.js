965ade732d249632feedf56ad69ec4155f388cde
Ho-Da-Dak <godqhr721@gmail.com>
2023-10-15 21:44:22 +0900

    Change axios code using form-data

--- quota/pages/login.js
+++ quota/pages/login.js
@@ -9,15 +9,27 @@ import Link from "next/link";
 export default function LoginPage() {
   const [formData, setFormData] = useState({ username: "", password: "" });
   const router = useRouter();
+
   function login() {
     const { username, password } = formData;
-    Axios.post("/api/login", { username, password }).then((res) => {
-      if (res.status === 200) {
-        router.push("/chat");
-      } else {
-        router.push("/chat");
-      }
-    });
+
+    const formDataToSend = new FormData();
+    formDataToSend.append("username", username);
+    formDataToSend.append("password", password);
+
+    Axios.post("http://localhost:8080/login", formDataToSend, { withCredentials: true })
+
+      .then((res) => {
+        if (res.status === 200) {
+          router.push("http://localhost:8080/articles");
+        } else {
+          router.push("/chat");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        // 에러 처리
+      });
   }
   function handleInputChange(event) {
     const { name, value } = event.target;
