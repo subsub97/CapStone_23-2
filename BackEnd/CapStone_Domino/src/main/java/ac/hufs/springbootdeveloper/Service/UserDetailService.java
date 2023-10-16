c6844c2add639ba9fdd1a9835b91531d871aedae
Ho-Da-Dak <godqhr721@gmail.com>
2023-10-17 01:14:33 +0900

    Created a user search service

---
+++ BackEnd/CapStone_Domino/src/main/java/ac/hufs/springbootdeveloper/Service/UserDetailService.java
@@ -0,0 +1,21 @@
+package ac.hufs.springbootdeveloper.Service;
+
+import ac.hufs.springbootdeveloper.domain.User;
+import ac.hufs.springbootdeveloper.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+
+public class UserDetailService implements UserDetailsService {
+
+     private final UserRepository userRepository;
+        @Override
+        public User loadUserByUsername(String email) {
+            return userRepository.findByEmail(email)
+                    .orElseThrow(()-> new IllegalArgumentException((email)));
+        }
+    }
+}
