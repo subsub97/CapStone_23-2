3fb7bb3c0cc71a303bd3ccddcdff6f1727cef877
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2023-10-17 20:53:34 +0900

    [2023-10-17] front

--- quota/pages/login.js
+++ quota/pages/login.js
@@ -7,18 +7,71 @@ import GoogleButton from "./GoogleButton";
 import Link from "next/link";
 
 export default function LoginPage() {
-  const [formData, setFormData] = useState({ username: "", password: "" });
+  const [formData, setFormData] = useState({ email: "", password: "" });
   const router = useRouter();
+
   function login() {
-    const { username, password } = formData;
-    Axios.post("/api/login", { username, password }).then((res) => {
-      if (res.status === 200) {
-        router.push("/chat");
-      } else {
-        router.push("/chat");
+    const { email, password } = formData;
+    Axios.post("http://localhost:8080/login", { email, password }).then(
+      (res) => {
+        if (res.status === 200) {
+          const accessToken = res.data.accessToken;
+          const refreshToken = res.data.refreshToken;
+          handleLoginSuccess(accessToken, refreshToken);
+        } else {
+          router.push("/login");
+        }
       }
-    });
+    );
   }
+
+  function handleLoginSuccess(accessToken, refreshToken) {
+    const storedAccessToken = accessToken;
+
+    localStorage.setItem("refreshToken", refreshToken);
+
+    Axios.post("http://localhost:8080/login", storedAccessToken, {
+      headers: {
+        Authorization: `Bearer ${storedAccessToken}`,
+      },
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          router.push("/chat");
+        } else if (res.status === 401) {
+          handleAccessTokenExpired(email, refreshToken);
+        } else {
+          router.push("/login");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function handleAccessTokenExpired(email, refreshToken) {
+    Axios.post("http://localhost:8080/refresh", { email, refreshToken })
+      .then((response) => {
+        const newAccessToken = response.data.accessToken;
+        Axios.post("http://localhost:8080/login", data, {
+          headers: {
+            Authorization: `Bearer ${newAccessToken}`,
+          },
+        })
+          .then((response) => {
+            handleLoginSuccess(newAccessToken, refreshToken);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch((error) => {
+        console.log(
+          "리프레시 토큰을 사용하여 새 액세스 토큰을 얻는 데 문제가 발생했습니다."
+        );
+      });
+  }
+
   function handleInputChange(event) {
     const { name, value } = event.target;
     setFormData({
@@ -32,9 +85,9 @@ export default function LoginPage() {
       <Form>
         <Form.Field inline>
           <input
-            name="username"
+            name="email"
             placeholder="ID"
-            value={formData.username}
+            value={formData.email}
             onChange={handleInputChange}
           />
         </Form.Field>
@@ -50,6 +103,9 @@ export default function LoginPage() {
         <Button color="blue" onClick={login}>
           Login
         </Button>
+        <Link href="/signup">
+          <Button color="green">Signup</Button>
+        </Link>
         <GoogleButton />
       </Form>
     </div>
